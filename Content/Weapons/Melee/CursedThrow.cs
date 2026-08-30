910743173a6b672587dfd0fa3a5e80032abe58e4
Andrew Robson <andrewrobson06@gmail.com>
2026-08-30 01:38:04 +0100

    added cursed throw weapon

--- Content/Weapons/Melee/CursedThrow.cs
+++ Content/Weapons/Melee/CursedThrow.cs
@@ -1,13 +1,44 @@
-﻿using Microsoft.Xna.Framework;
+﻿using BeefsMod.Content.Weapons.Projectiles.Weapons.Melee;
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-using Terraria;
-using Terraria.DataStructures;
 
 namespace BeefsMod.Content.Weapons.Melee
 {
-    internal class CursedThrow : ModItem
+    public class CursedThrow : ModItem
     {
+        public override void SetDefaults()
+        {
+            Item.width = 24;
+            Item.height = 24;
 
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.useTime = 25;
+            Item.useAnimation = 25;
+            Item.noMelee = true;
+            Item.noUseGraphic = true;
+            Item.UseSound = SoundID.Item1;
+
+            Item.damage = 44;
+            Item.DamageType = DamageClass.MeleeNoSpeed;
+            Item.knockBack = 2.3f;
+            Item.channel = true;
+            Item.rare = ItemRarityID.LightRed;
+            Item.value = Item.sellPrice(gold: 5, silver: 20);
+
+            Item.shoot = ModContent.ProjectileType<CursedThrowProjectile>();
+            Item.shootSpeed = 16f;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.HelFire)
+                .AddIngredient(ItemID.CursedFlame, 15)
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
     }
 }
---
+++ Content/Weapons/Projectiles/Weapons/Melee/CursedThrowProjectile.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BeefsMod.Content.Weapons.Projectiles.Weapons.Melee
+{
+    public class CursedThrowProjectile : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.YoyosLifeTimeMultiplier[Type] = 17f;
+
+            ProjectileID.Sets.YoyosMaximumRange[Type] = 280f;
+
+            ProjectileID.Sets.YoyosTopSpeed[Type] = 15f;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 16;
+            Projectile.height = 16;
+
+            Projectile.aiStyle = ProjAIStyleID.Yoyo;
+
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.MeleeNoSpeed;
+            Projectile.penetrate = -1;
+        }
+
+        public override void PostAI()
+        {
+            if (Main.rand.NextBool(5))
+                Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.CursedTorch);
+
+        }
+
+        public override void PostDraw(Color lightColor)
+        {
+            Main.instance.LoadProjectile(540);
+            Texture2D bloomTex = TextureAssets.Projectile[540].Value;
+
+            Main.spriteBatch.End();
+            Main.spriteBatch.Begin(default, BlendState.Additive, default, default, default, null, Main.GameViewMatrix.TransformationMatrix);
+
+            Main.spriteBatch.Draw(bloomTex, Projectile.Center - new Vector2(0, 0).RotatedBy(0) - Main.screenPosition,
+                   null, new Color(55, 200, 26, 150), 0, bloomTex.Size() / 2f, 0.4f, 0f, 0f);
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            target.AddBuff(BuffID.CursedInferno, 420);
+        }
+    }
+
+}

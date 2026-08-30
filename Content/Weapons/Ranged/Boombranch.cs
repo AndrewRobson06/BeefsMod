b5f141776f5c6c2bf111fea271abaa92b5f3eb4b
Andrew Robson <andrewrobson06@gmail.com>
2026-08-30 18:49:13 +0100

    added boombranch weapon

--- Content/Weapons/Ranged/Boombranch.cs
+++ Content/Weapons/Ranged/Boombranch.cs
@@ -1,12 +1,83 @@
 ﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
-using Terraria;
-using Terraria.DataStructures;
 
 namespace BeefsMod.Content.Weapons.Ranged
 {
-    internal class Boombranch : ModItem
+    public class Boombranch : ModItem
     {
+        public override void SetDefaults()
+        {
+            Item.width = 64;
+            Item.height = 16;
+
+            Item.rare = ItemRarityID.LightRed;
+
+            Item.useTime = 8;
+            Item.useAnimation = 16;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.reuseDelay = 50;
+            Item.consumeAmmoOnLastShotOnly = true;
+            Item.autoReuse = false;
+            Item.UseSound = SoundID.Item36;
+
+            Item.DamageType = DamageClass.Ranged;
+            Item.damage = 18;
+            Item.knockBack = 6f;
+            Item.noMelee = true;
+
+            Item.shoot = ProjectileID.PurificationPowder; //i dunno why but do this
+            Item.shootSpeed = 10f;
+            Item.useAmmo = AmmoID.Bullet;
+        }
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            SoundEngine.PlaySound(SoundID.Item38 with { Volume = 0.7f });
+
+            int NumProjectiles = Main.rand.Next(4, 5);
+
+            for (int i = 0; i < NumProjectiles; i++)
+            {
+                Vector2 newVelocity = velocity.RotatedByRandom(MathHelper.ToRadians(15));
+
+                newVelocity *= 1f - Main.rand.NextFloat(0.3f);
+
+                Projectile.NewProjectileDirect(source, position, newVelocity, type, damage, knockback, player.whoAmI);
+            }
+
+            return false;
+        }
+
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback) {
+            Vector2 muzzleOffset = Vector2.Normalize(velocity) * 25f;
+
+            if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0)) {
+                position += muzzleOffset;
+            }
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.Boomstick)
+                .AddIngredient(ItemID.MythrilBar, 12)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
+
+            CreateRecipe()
+                .AddIngredient(ItemID.Boomstick)
+                .AddIngredient(ItemID.OrichalcumBar, 12)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
+        }
+
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(0f, 0f);
+        }
     }
 }

ca7d2b8f053bcdfc7ddb4cbfb91366fab70fc23c
Andrew Robson <andrewrobson06@gmail.com>
2026-08-30 15:43:19 +0100

    began working on crimtane bullet

--- Content/Weapons/Ranged/Ammo/CrimtaneBullet.cs
+++ Content/Weapons/Ranged/Ammo/CrimtaneBullet.cs
@@ -1,12 +1,40 @@
 ﻿using Microsoft.Xna.Framework;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-using Terraria;
-using Terraria.DataStructures;
 
 namespace BeefsMod.Content.Weapons.Ranged.Ammo
 {
-    internal class CrimtaneBullet : ModItem
+    public class CrimtaneBullet : ModItem
     {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 99;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 10;
+            Item.DamageType = DamageClass.Ranged;
+            Item.width = 8;
+            Item.height = 8;
+            Item.maxStack = Item.CommonMaxStack;
+            Item.consumable = true; // This marks the item as consumable, making it automatically be consumed when it's used as ammunition, or something else, if possible.
+            Item.knockBack = 1.5f;
+            Item.value = 10;
+            Item.rare = ItemRarityID.Green;
+            Item.shoot = ModContent.ProjectileType<CrimtaneBulletProjectile>(); // The projectile that weapons fire when using this item as ammunition.
+            Item.shootSpeed = 5f; // The speed of the projectile. This value equivalent to Silver Bullet since ExampleBullet's Projectile.extraUpdates is 1.
+            Item.ammo = AmmoID.Bullet; // The ammo class this ammo belongs to.
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe(70)
+                .AddIngredient(ItemID.MusketBall, 70)
+                .AddIngredient(ItemID.CrimtaneBar)
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
     }
 }

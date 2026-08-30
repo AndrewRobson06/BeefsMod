4bb5158f0ac80c14f72f00c2ed82a0de357cb885
Andrew Robson <andrewrobson06@gmail.com>
2026-08-30 01:48:39 +0100

    added golden sling weapon

--- Content/Weapons/Melee/GoldenSling.cs
+++ Content/Weapons/Melee/GoldenSling.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.Xna.Framework;
-using Terraria.ID;
-using Terraria.ModLoader;
+﻿using BeefsMod.Content.Weapons.Projectiles.Weapons.Melee;
+using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace BeefsMod.Content.Weapons.Melee
 {
-    internal class GoldenSling : ModItem
+    public class GoldenSling : ModItem
     {
+        public override void SetDefaults()
+        {
+            Item.width = 24;
+            Item.height = 24;
+
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.useTime = 25;
+            Item.useAnimation = 25;
+            Item.noMelee = true;
+            Item.noUseGraphic = true;
+            Item.UseSound = SoundID.Item1;
+
+            Item.damage = 38;
+            Item.DamageType = DamageClass.MeleeNoSpeed;
+            Item.knockBack = 2.3f;
+            Item.channel = true;
+            Item.rare = ItemRarityID.LightRed;
+            Item.value = Item.sellPrice(gold: 5, silver: 35);
+
+            Item.shoot = ModContent.ProjectileType<GoldenSlingProjectile>();
+            Item.shootSpeed = 16f;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.HelFire)
+                .AddIngredient(ItemID.Ichor, 15)
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
     }
 }

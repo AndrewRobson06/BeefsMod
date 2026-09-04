ec2dcfe471feb79d952d48c3de130a41c2821a46
Andrew Robson <andrewrobson06@gmail.com>
2026-09-04 23:14:49 +0100

    begging work on forbidden centipede boss

---
+++ Content/NPCs/Boss/ForbiddenCentipede/ForbiddenCentipede.cs
@@ -0,0 +1,142 @@
+﻿using System.IO;
+using Terraria;
+using Terraria.GameContent.Bestiary;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using BeefsMod.Content.NPCs;
+
+namespace BeefsMod.Content.NPCs.Boss.ForbiddenCentipede
+{
+    public class ForbiddenCentipedeHead : WormHead
+    {
+        public override int BodyType => ModContent.NPCType<ForbiddenCentipedeBody>();
+
+        public override int TailType => ModContent.NPCType<ForbiddenCentipedeTail>();
+
+        public override void SetStaticDefaults()
+        {
+            var drawModifier = new NPCID.Sets.NPCBestiaryDrawModifiers()
+            { // Influences how the NPC looks in the Bestiary
+                CustomTexturePath = "BeefsMod/Content/NPCs/Boss/ForbiddenCentipede/ForbiddenCentipedeHead", // If the NPC is multiple parts like a worm, a custom texture for the Bestiary is encouraged.
+                Position = new Vector2(40f, 24f),
+                PortraitPositionXOverride = 0f,
+                PortraitPositionYOverride = 12f
+            };
+            NPCID.Sets.NPCBestiaryDrawOffset.Add(NPC.type, drawModifier);
+        }
+
+        public override void SetDefaults()
+        {
+            // Head is 10 defense, body 20, tail 30.
+            NPC.CloneDefaults(NPCID.DiggerHead);
+            NPC.aiStyle = -1;
+
+
+        }
+
+        public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+        {
+            // We can use AddRange instead of calling Add multiple times in order to add multiple items at once
+            bestiaryEntry.Info.AddRange([
+				// Sets the spawning conditions of this NPC that is listed in the bestiary.
+				BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Biomes.Desert,
+
+
+				// Sets the description of this NPC that is listed in the bestiary.
+				new FlavorTextBestiaryInfoElement("ForbiddenCentipede")
+            ]);
+        }
+
+        public override void Init()
+        {
+            // Set the segment variance
+            // If you want the segment length to be constant, set these two properties to the same value
+            MinSegmentLength = 50;
+            MaxSegmentLength = 50;
+
+            CommonWormInit(this);
+        }
+
+        internal static void CommonWormInit(Worm worm)
+        {
+            // These two properties handle the movement of the worm
+            worm.MoveSpeed = 5.5f;
+            worm.Acceleration = 0.045f;
+        }
+
+        private int attackCounter;
+        public override void SendExtraAI(BinaryWriter writer)
+        {
+            writer.Write(attackCounter);
+        }
+
+        public override void ReceiveExtraAI(BinaryReader reader)
+        {
+            attackCounter = reader.ReadInt32();
+        }
+
+        public override void AI()
+        {
+            if (Main.netMode != NetmodeID.MultiplayerClient)
+            {
+                if (attackCounter > 0)
+                {
+                    attackCounter--; // tick down the attack counter.
+                }
+
+                Player target = Main.player[NPC.target];
+                // If the attack counter is 0, this NPC is less than 12.5 tiles away from its target, and has a path to the target unobstructed by blocks, summon a projectile.
+                if (attackCounter <= 0 && Vector2.Distance(NPC.Center, target.Center) < 200 && Collision.CanHit(NPC.Center, 1, 1, target.Center, 1, 1))
+                {
+                    Vector2 direction = (target.Center - NPC.Center).SafeNormalize(Vector2.UnitX);
+                    direction = direction.RotatedByRandom(MathHelper.ToRadians(10));
+
+                    int projectile = Projectile.NewProjectile(NPC.GetSource_FromThis(), NPC.Center, direction * 1, ProjectileID.ShadowBeamHostile, 5, 0, Main.myPlayer);
+                    Main.projectile[projectile].timeLeft = 300;
+                    attackCounter = 500;
+                    NPC.netUpdate = true;
+                }
+            }
+        }
+    }
+
+    internal class ForbiddenCentipedeBody : WormBody
+    {
+        public override void SetStaticDefaults()
+        {
+            NPCID.Sets.NPCBestiaryDrawModifiers value = new NPCID.Sets.NPCBestiaryDrawModifiers()
+            {
+                Hide = true // Hides this NPC from the Bestiary, useful for multi-part NPCs whom you only want one entry.
+            };
+            NPCID.Sets.NPCBestiaryDrawOffset.Add(Type, value);
+            NPCID.Sets.RespawnEnemyID[Type] = ModContent.NPCType<ForbiddenCentipedeHead>();
+        }
+
+        
+
+        public override void Init()
+        {
+            ForbiddenCentipedeHead.CommonWormInit(this);
+        }
+    }
+
+    internal class ForbiddenCentipedeTail : WormTail
+    {
+        public override void SetStaticDefaults()
+        {
+            NPCID.Sets.NPCBestiaryDrawModifiers value = new NPCID.Sets.NPCBestiaryDrawModifiers()
+            {
+                Hide = true // Hides this NPC from the Bestiary, useful for multi-part NPCs whom you only want one entry.
+            };
+            NPCID.Sets.NPCBestiaryDrawOffset.Add(Type, value);
+            NPCID.Sets.RespawnEnemyID[Type] = ModContent.NPCType<ForbiddenCentipedeHead> ();
+        }
+
+
+        public override void Init()
+        {
+            ForbiddenCentipedeHead.CommonWormInit(this);
+        }
+    }
+}

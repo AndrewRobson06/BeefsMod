4cdf059c8620f772e57d6e5f2aa55376c8cbca65
Andrew Robson <andrewrobson06@gmail.com>
2026-09-07 22:15:35 +0100

    added shroom powered assult rifle weapon

---
+++ Content/Weapons/Ranged/ShroomPoweredAssultRifle.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+namespace BeefsMod.Content.Weapons.Ranged
+{
+    public class ShroomPoweredAssultRifle : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 56;
+            Item.height = 20;
+
+            Item.rare = ItemRarityID.Yellow;
+
+            Item.value = Item.sellPrice(gold: 18, silver: 39);
+
+            Item.useTime = 1;
+            Item.useAnimation = 12;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.reuseDelay = 20;
+            Item.consumeAmmoOnLastShotOnly = true;
+            Item.autoReuse = false;
+            Item.UseSound = SoundID.Item11;
+
+            Item.DamageType = DamageClass.Ranged;
+            Item.damage = 32;
+            Item.knockBack = 6f;
+            Item.noMelee = true;
+
+            Item.shoot = ProjectileID.PurificationPowder; //i dunno why but do this
+            Item.shootSpeed = 10f;
+            Item.useAmmo = AmmoID.Bullet;
+        }
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            SoundEngine.PlaySound(SoundID.Item11 with { Volume = 0.7f });
+
+            Projectile.NewProjectileDirect(source, position, velocity, type, damage, knockback, player.whoAmI);
+            return false;
+        }
+
+        public override bool CanConsumeAmmo(Item ammo, Player player)
+        {
+            return Main.rand.NextFloat() >= 0.25f; // 18% chance to not consume ammo
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ModContent.ItemType<BioPoweredRifle>())
+                .AddIngredient(ItemID.ShroomiteBar, 12)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
+        }
+    }
+}

7f8f94033af75ddfcfcfc300b92c85b3f72b8e6c
Andrew Robson <andrewrobson06@gmail.com>
2026-09-07 18:53:52 +0100

    added pygmy shield and began work on venom sludge tome weapon

---
+++ Content/Accessories/PygmyShield.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BeefsMod.Content.Accessories
+{
+    [AutoloadEquip(EquipType.Shield)]
+    public class PygmyShield : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 30;
+            Item.height = 28;
+
+            Item.value = Item.sellPrice(gold: 5, silver: 75);
+
+            Item.rare = ItemRarityID.Lime;
+
+            Item.defense = 1;
+            Item.accessory = true;
+            
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.noKnockback = true;
+            player.maxMinions += 1;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.CobaltShield)
+                .AddIngredient(ItemID.PygmyNecklace)
+                .AddTile(TileID.TinkerersWorkbench)
+                .Register();
+                
+        }
+    }
+}
---
+++ Content/Weapons/Magic/VenomSludgeTome.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace BeefsMod.Content.Weapons.Magic
+{
+    public class VenomSludgeTome : ModItem
+    {
+
+    }
+}

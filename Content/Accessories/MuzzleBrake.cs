fa97415fb0860163c94adec1dae5179123d94135
Andrew Robson <andrewrobson06@gmail.com>
2026-09-12 17:34:12 +0100

    added muzzle break accessories

---
+++ Content/Accessories/MuzzleBrake.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BeefsMod.Content.Accessories
+{
+    public class MuzzleBrake : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 42;
+            Item.height = 20;
+
+            Item.value = Item.buyPrice(gold: 6);
+
+            Item.rare = ItemRarityID.Blue;
+
+            Item.accessory = true;
+
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.bulletDamage += 0.5f;
+        }
+
+        
+    }
+}
+
---
+++ Core/GlobalNPCs/AddToShop.cs
@@ -0,0 +1,21 @@
+﻿using BeefsMod.Content.Accessories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BeefsMod.Core.GlobalNPCs
+{
+    public class AddToShop : GlobalNPC
+    {
+        public override void ModifyShop(NPCShop shop)
+        {
+            if (shop.NpcType == NPCID.ArmsDealer)
+                shop.Add(ModContent.ItemType<MuzzleBrake>());
+        }
+    }
+}

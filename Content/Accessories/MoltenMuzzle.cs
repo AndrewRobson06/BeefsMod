252e9ecb60e6fe88ed6bee6ad12377f8c2887202
Andrew Robson <andrewrobson06@gmail.com>
2026-09-14 08:52:11 +0100

    Began work on molten muzzle accessory, need to fix so that only bullets are set on fire, all projectiles are set on fire right now

---
+++ Content/Accessories/MoltenMuzzle.cs
@@ -0,0 +1,41 @@
+﻿using BeefsMod.Core.ModPlayers;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BeefsMod.Content.Accessories
+{
+    public class MoltenMuzzle : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 42;
+            Item.height = 20;
+
+            Item.value = Item.buyPrice(gold: 3, silver: 20);
+
+            Item.rare = ItemRarityID.Orange;
+
+            Item.accessory = true;
+
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.bulletDamage += 0.6f;
+            player.GetModPlayer<MoltenMuzzleBonus>().hasMoltenMuzzle = true;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient<MuzzleBrake>()
+                .AddIngredient(ItemID.MagmaStone)
+                .AddTile(TileID.TinkerersWorkbench)
+                .Register();
+        }
+    }
+}
+
+
---
+++ Core/ModPlayers/MoltenMuzzleBonus.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BeefsMod.Core.ModPlayers
+{
+    public class MoltenMuzzleBonus : ModPlayer
+    {
+        public bool hasMoltenMuzzle;
+
+        public override void ResetEffects()
+        {
+            hasMoltenMuzzle = false;
+        }
+    }
+
+    /*public class MoltenMuzzleApplyFireGlobal : GlobalItem
+    {
+        public override void OnHitNPC(Item item, Player player, NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            if (player.GetModPlayer<MoltenMuzzleBonus>().hasMoltenMuzzle && Item.ammo = AmmoID.Bullet)
+                target.AddBuff(BuffID.Venom, 420);
+        }
+    }*/
+
+    public class MoltenMuzzleApplyFireGlobalProjectile : GlobalProjectile //fix so only bullets are set on fire
+    {
+        public override void OnHitNPC(Projectile projectile, NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            if (Main.player[projectile.owner].GetModPlayer<MoltenMuzzleBonus>().hasMoltenMuzzle)
+                target.AddBuff(BuffID.OnFire3, 420);
+        }
+    }
+
+}
+
+

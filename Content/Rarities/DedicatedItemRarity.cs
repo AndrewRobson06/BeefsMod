825ee9352f62b411cf84d3f70f1f18dc626c8526
Andrew Robson <andrewrobson06@gmail.com>
2026-09-14 14:24:45 +0100

    added dedicated item rarity and beginning to apply it

---
+++ Content/Rarities/DedicatedItemRarity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace BeefsMod.Content.Rarities
+{
+    public class DedicatedItemRarity : ModRarity
+    {
+        public override Color RarityColor => new Color(223, 88, 19);
+ 
+    }
+}

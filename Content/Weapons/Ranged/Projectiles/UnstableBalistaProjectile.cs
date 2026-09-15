c49a40118c623304d36961f882987f7eefeee378
Andrew Robson <andrewrobson06@gmail.com>
2026-09-15 01:42:39 +0100

    unstable ballista working but needs some visula touches and finishing off

---
+++ Content/Weapons/Ranged/Projectiles/UnstableBalistaProjectile.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Xna.Framework;
+using Mono.Cecil;
+using System;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent.Tile_Entities;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace BeefsMod.Content.Weapons.Ranged.Projectiles
+{
+    public class UnstableBalistaProjectile : ModProjectile
+    {
+
+        public ref float ChargeTimer => ref Projectile.ai[0];
+
+        public const float MaxCharge = 300f;
+
+        public int ArrowAmount = 1;
+        public override void SetDefaults()
+        {
+            Projectile.width = 74;
+            Projectile.height = 76;
+            Projectile.tileCollide = false;
+            Projectile.friendly = false;
+            Projectile.aiStyle = -1;
+        }
+
+        public override void AI()
+        {
+            Player player = Main.player[Projectile.owner];
+
+            if (player.dead || !player.active)
+            {
+                Projectile.Kill();
+                return;
+            }
+
+
+            Projectile.Center = player.MountedCenter;
+            player.heldProj = Projectile.whoAmI;
+            player.itemTime = 2;
+            player.itemAnimation = 1;
+
+            Vector2 aim = Main.MouseWorld - player.MountedCenter;
+            aim.Normalize();
+            player.ChangeDir(aim.X > 0 ? 1 : -1);
+
+            if (player.channel)
+            {
+                if (ChargeTimer < MaxCharge)
+                {
+                    ChargeTimer++;
+
+                    if (ChargeTimer % 6 == 0)
+                    {
+                        //add 1 arrow every 6 ticks so 10 a second
+                        ArrowAmount++;
+                        if (ArrowAmount > 50)
+                            ArrowAmount = 50;
+                    } 
+
+                    if (ChargeTimer == 60 || ChargeTimer == 120 || ChargeTimer == 180 || ChargeTimer == 240)
+                        SoundEngine.PlaySound(SoundID.Unlock);
+
+                    if (ChargeTimer == MaxCharge)
+                    {
+                        SoundEngine.PlaySound(SoundID.MaxMana);
+                    }
+                }
+            }
+            else
+            {
+                FireShot(aim);
+            }
+        }
+
+        public void FireShot(Vector2 velocity)
+        {
+            Player player = Main.player[Projectile.owner];
+
+            SoundEngine.PlaySound(SoundID.Item40 with { Volume = 2.5f });
+
+            if (Main.myPlayer == Projectile.owner)
+            {
+                for (int i = 0; i < ArrowAmount; i++)
+                {
+                    Vector2 newVelocity = velocity.RotatedByRandom(MathHelper.ToRadians(15));
+
+                    newVelocity *= 1f - Main.rand.NextFloat(0.4f);
+
+                    Projectile.NewProjectileDirect(Projectile.GetSource_FromThis(), player.MountedCenter, newVelocity * 20f, ProjectileID.WoodenArrowFriendly, Projectile.damage, Projectile.knockBack, Projectile.owner);
+                }
+                
+            }
+            Projectile.Kill();
+        }
+    }
+}
--- Content/Weapons/Ranged/UnstableBalista.cs
+++ Content/Weapons/Ranged/UnstableBalista.cs
@@ -1,12 +1,95 @@
-﻿using System;
+﻿using BeefsMod.Content.Weapons.Ranged.Projectiles;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace BeefsMod.Content.Weapons.Ranged
 {
-    internal class UnstableBalista
+    public class UnstableBalista : ModItem
     {
+
+
+        public const int HoldOutDistance = 20;
+
+        public override void SetDefaults()
+        {
+            Item.width = 74;
+            Item.height = 76;
+
+            Item.rare = ItemRarityID.LightPurple;
+
+            Item.value = Item.sellPrice(gold: 30, silver:20);
+
+            Item.useTime = 10;
+            Item.useAnimation = 10;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.autoReuse = true;
+            //Item.UseSound = SoundID.Item5;
+            //Item.reuseDelay = 30;
+
+            Item.DamageType = DamageClass.Ranged;
+            Item.damage = 70;
+            Item.knockBack = 6f;
+            Item.noMelee = true;
+            Item.channel = true;
+
+            Item.shoot = ModContent.ProjectileType<UnstableBalistaProjectile>(); //i dunno why but do this
+            Item.shootSpeed = 10f;
+            Item.noUseGraphic = true;
+        }
+
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            type = ModContent.ProjectileType<UnstableBalistaProjectile>();
+
+            velocity = Vector2.Normalize(velocity) * HoldOutDistance;
+
+            Projectile.NewProjectile(source, position, velocity, type, damage, knockback, Main.myPlayer);
+
+            return false;
+        }
+
+
+        /*public override bool CanConsumeAmmo(Item ammo, Player player)
+        {
+            if (player.ItemTimeIsZero)
+            {
+                return false;
+            }
+            return true;
+        }*/
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.HallowedBar, 20)
+                .AddIngredient(ItemID.AdamantiteBar, 15)
+                .AddIngredient(ItemID.SoulofFright, 5)
+                .AddIngredient(ItemID.SoulofSight, 5)
+                .AddIngredient(ItemID.SoulofMight, 5)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
+
+            CreateRecipe()
+                .AddIngredient(ItemID.HallowedBar, 20)
+                .AddIngredient(ItemID.TitaniumBar, 15)
+                .AddIngredient(ItemID.SoulofFright, 5)
+                .AddIngredient(ItemID.SoulofSight, 5)
+                .AddIngredient(ItemID.SoulofMight, 5)
+                .AddTile(TileID.MythrilAnvil)
+                .Register();
+        }
     }
 }

881e80494dfeee56e88a489929d0dc00fba2ef09
Andrew Robson <andrewrobson06@gmail.com>
2026-09-17 12:02:42 +0100

    added bone dart ammo

---
+++ Content/Weapons/Ranged/Ammo/BoneDart.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BeefsMod.Content.Weapons.Ranged.Ammo
+{
+    public class BoneDart : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 99;
+        }
+        public override void SetDefaults()
+        {
+            Item.damage = 12;
+            Item.DamageType = DamageClass.Ranged;
+            Item.width = 10;
+            Item.height = 24;
+            Item.maxStack = Item.CommonMaxStack;
+            Item.consumable = true; // This marks the item as consumable, making it automatically be consumed when it's used as ammunition, or something else, if possible.
+            Item.knockBack = 1.5f;
+            Item.value = Item.sellPrice(copper: 1);
+            Item.rare = ItemRarityID.Blue;
+            Item.shoot = ModContent.ProjectileType<BoneDartProjectile>(); // The projectile that weapons fire when using this item as ammunition.
+            Item.shootSpeed = 10f; // The speed of the projectile. This value equivalent to Silver Bullet since ExampleBullet's Projectile.extraUpdates is 1.
+            Item.ammo = AmmoID.Dart; // The ammo class this ammo belongs to.
+            
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe(100)
+                .AddIngredient(ItemID.Bone, 100)
+                .AddTile(TileID.WorkBenches)
+                .Register();
+
+        }
+    }
+}
---
+++ Content/Weapons/Ranged/Ammo/BoneDartProjectile.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BeefsMod.Content.Weapons.Ranged.Ammo
+{
+    public class BoneDartProjectile : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 8; // The width of projectile hitbox
+            Projectile.height = 8; // The height of projectile hitbox
+            Projectile.aiStyle = 1; // The ai style of the projectile, please reference the source code of Terraria
+            Projectile.friendly = true; // Can the projectile deal damage to enemies?
+            Projectile.hostile = false; // Can the projectile deal damage to the player?
+            Projectile.DamageType = DamageClass.Ranged; // Is the projectile shoot by a ranged weapon?
+            Projectile.penetrate = 2; // How many monsters the projectile can penetrate. (OnTileCollide below also decrements penetrate for bounces as well)
+            Projectile.timeLeft = 600; // The live time for the projectile (60 = 1 second, so 600 is 10 seconds)
+            Projectile.ignoreWater = true; // Does the projectile's speed be influenced by water?
+            Projectile.tileCollide = true; // Can the projectile collide with tiles?
+            Projectile.extraUpdates = 2; // Set to above 0 if you want the projectile to update multiple time in a frame
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = 10;
+
+
+
+            //AIType = ProjectileID.PoisonDart; // Act exactly like default Bullet
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            return true;
+        }
+    }
+}
